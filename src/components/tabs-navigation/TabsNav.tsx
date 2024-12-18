2f2e7a24d0b5e68cd525e525a50e532ba4b58271
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-12-18 14:28:04 -0300

    Enhance TabsNav component with additional tab triggers and icons for better navigation

--- src/components/tabs-navigation/TabsNav.tsx
+++ src/components/tabs-navigation/TabsNav.tsx
@@ -1,23 +1,44 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { LayoutDashboardIcon } from "lucide-react";
+import {
+  LayoutDashboardIcon,
+  FileTextIcon,
+  User2Icon,
+  HelpCircleIcon,
+  SearchIcon,
+} from "lucide-react";
 
 const TabsNav = () => {
   return (
     <Tabs
-      defaultValue="tab-1"
+      defaultValue="visao-geral"
       onValueChange={value => console.log(value)}
       className="flex flex-col h-full w-full mr-4"
     >
-      <TabsList className="flex-shrink-0 bg-green text-white ">
-        <TabsTrigger value="visao-geral">
+      <TabsList className="flex-shrink-0 bg-green text-white md:text-lg tracking-wide">
+        <TabsTrigger className="flex-between gap-2" value="visao-geral">
           <LayoutDashboardIcon size={18} />
-          <span className="hidden">Visão Geral</span>
+          <span className="hidden md:block">Visão Geral</span>
         </TabsTrigger>
-        <TabsTrigger value="nfse">Notas Fiscais</TabsTrigger>
-        <TabsTrigger value="contribuintes">Contribuintes</TabsTrigger>
-        <TabsTrigger value="tab-4">Tab 4</TabsTrigger>
-        <TabsTrigger value="tab-5">Tab 5</TabsTrigger>
-        <TabsTrigger value="tab-6">Tab 6</TabsTrigger>
+        <TabsTrigger className="flex-between gap-2" value="nfse">
+          <FileTextIcon size={18} />
+          <span className="hidden md:block">Notas Fiscais</span>
+        </TabsTrigger>
+        <TabsTrigger className="flex-between gap-2" value="contribuintes">
+          <User2Icon size={18} />
+          <span className="hidden md:block">Contribuintes</span>
+        </TabsTrigger>
+        <TabsTrigger className="flex-between gap-2" value="consultas">
+          <SearchIcon size={18} />
+          <span className="hidden md:block">Consultas</span>
+        </TabsTrigger>
+        <TabsTrigger className="flex-between gap-2" value="tab-5">
+          <HelpCircleIcon size={18} />
+          <span className="hidden md:block">Tab 5</span>
+        </TabsTrigger>
+        <TabsTrigger className="flex-between gap-2" value="tab-6">
+          <HelpCircleIcon size={18} />
+          <span className="hidden md:block ">Tab 6</span>
+        </TabsTrigger>
       </TabsList>
       <main className="flex-grow text-center">
         <TabsContent value="visao-geral">
@@ -27,7 +48,7 @@ const TabsNav = () => {
         <TabsContent value="contribuintes">
           Contribuintes Dashboard content
         </TabsContent>
-        <TabsContent value="tab-4">Tab 4 content</TabsContent>
+        <TabsContent value="consultas">Painel Consultas</TabsContent>
         <TabsContent value="tab-5">Tab 5 content</TabsContent>
         <TabsContent value="tab-6">Tab 6 content</TabsContent>
       </main>
