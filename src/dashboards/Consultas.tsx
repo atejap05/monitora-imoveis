c3c631c259025d07a67b5ee27a7601a026bc252c
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-12-19 11:33:10 -0300

    Add Consultas component for displaying Consultas dashboard content

---
+++ src/dashboards/Consultas.tsx
@@ -0,0 +1,11 @@
+import { TabsContent } from "@/components/ui/tabs";
+
+const Consultas = () => {
+  return (
+    <TabsContent value="consultas">
+      <div className="p-4">Painel Consultas</div>
+    </TabsContent>
+  );
+};
+
+export default Consultas;
