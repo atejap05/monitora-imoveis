18c19e0f2a9d01fed7adff508a151dd501c00c42
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-12-19 11:33:05 -0300

    Add Contribuintes component for displaying Contribuintes dashboard content

---
+++ src/dashboards/Contribuintes.tsx
@@ -0,0 +1,11 @@
+import { TabsContent } from "@/components/ui/tabs";
+
+const Contribuintes = () => {
+  return (
+    <TabsContent value="contribuintes">
+      <div className="p-4">Contribuintes Dashboard content</div>
+    </TabsContent>
+  );
+};
+
+export default Contribuintes;
