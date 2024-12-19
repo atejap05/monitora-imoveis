c35b84a7e58da842f6f98939eac08a6e214ca930
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-12-19 11:32:48 -0300

    Add VisaoGeral component for dashboard overview

---
+++ src/dashboards/VisaoGeral.tsx
@@ -0,0 +1,11 @@
+import { TabsContent } from "@/components/ui/tabs";
+
+const VisaoGeral = () => {
+  return (
+    <TabsContent value="visao-geral">
+      <div className="p-4">Visão Geral Dashboard content</div>
+    </TabsContent>
+  );
+};
+
+export default VisaoGeral;
