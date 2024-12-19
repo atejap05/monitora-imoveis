4bd0ca52b10445e4848b37519d36172b590cd7e2
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-12-19 11:32:54 -0300

    Add NotasFiscais component for displaying Notas Fiscais dashboard content

---
+++ src/dashboards/NotasFiscais.tsx
@@ -0,0 +1,11 @@
+import { TabsContent } from "@/components/ui/tabs";
+
+const NotasFiscais = () => {
+  return (
+    <TabsContent value="nfse">
+      <div className="p-4">Notas Fiscais Dashboard content</div>
+    </TabsContent>
+  );
+};
+
+export default NotasFiscais;
