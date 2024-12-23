fe41852dce50e885ff5a9b8ac288767c4ed614ce
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-12-23 16:02:42 -0300

    Add RadioGroup component and utility functions for state management

--- src/forms/form-contribuintes/FormContribuintes.tsx
+++ src/forms/form-contribuintes/FormContribuintes.tsx
@@ -1,26 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
   Form,
-  // FormControl,
+  FormControl,
   // FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { MultiSelect } from "@/components/ui/multi-select";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { findUFCodigo, selectUFOptions } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { useQuery } from "@tanstack/react-query";
 
+//////////// FormSchema ////////////
 const FormSchema = z.object({
-  uf: z.array(z.string()).nonempty("UF é obrigatório"),
+  uf: z.string().nonempty("UF é obrigatório"),
+  ano: z.array(z.string()).optional(),
+  municipio: z.string().nonempty("Município é obrigatório"),
+  filter_option: z
+    .enum(["uf", "municipio", "regiao", "todos"])
+    .default("todos"),
 });
+
+const fecthMunicipioByUf = async (uf: string) => {
+  const ufCodigo = await findUFCodigo(uf);
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufCodigo}/municipios`;
+  const response = await fetch(url);
+  return await response.json();
+};
 
 export function FormContribuintes() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
+    defaultValues: {
+      filter_option: "todos",
+    },
+  });
+
+  const result = useQuery({
+    queryKey: ["fecth-municipios", form.watch("uf")],
+    queryFn: () => fecthMunicipioByUf(form.watch("uf")),
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
@@ -32,37 +64,159 @@ export function FormContribuintes() {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <FormField
           control={form.control}
-          name="uf"
+          name="filter_option"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Filtrar por:</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="grid grid-cols-2 gap-4"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="todos" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Todos</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="uf" />
+                    </FormControl>
+                    <FormLabel className="font-normal">UF</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="municipio" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Município</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="regiao" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Região</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Separator />
+        {form.watch("filter_option") === "uf" && (
+          <FormField
+            control={form.control}
+            name="uf"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-green font-bold">UF</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecione um Estado" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {selectUFOptions.map(option => (
+                      <SelectItem key={option.value} value={option.value}>
+                        {option.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        )}
+        {form.watch("filter_option") === "municipio" && (
+          <>
+            <FormField
+              control={form.control}
+              name="uf"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-green font-bold">UF</FormLabel>
+                  <Select
+                    onValueChange={value => {
+                      field.onChange(value);
+                      result.refetch();
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione um Estado" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {selectUFOptions.map(option => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="municipio"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-green font-bold">
+                    Município
+                  </FormLabel>
+                  <Select
+                    disabled={result.isLoading}
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Selecione o Município">
+                        {result.isLoading
+                          ? "Carregando..."
+                          : field.value || "Selecione o Município"}
+                      </SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                      {result.data?.map((municipio: any) => (
+                        <SelectItem key={municipio.id} value={municipio.nome}>
+                          {municipio.nome}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </>
+        )}
+        <FormField
+          control={form.control}
+          name="ano"
           render={({ field }) => (
             <FormItem>
-              <FormLabel className="text-green font-bold">UF</FormLabel>
-              <MultiSelect
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                placeholder="Selecione o Estado"
-                options={[
-                  { label: "SP", value: "SP" },
-                  { label: "BA", value: "BA" },
-                  { label: "PI", value: "PI" },
-                ]}
-              />
-              {/* <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger className="bg-white min-w-56">
-                    <SelectValue placeholder="Selecione o Estado" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="SP">SP</SelectItem>
-                  <SelectItem value="BA">BA</SelectItem>
-                  <SelectItem value="PI">PI</SelectItem>
-                </SelectContent>
-              </Select> */}
+              <FormLabel className="text-green font-bold">Ano</FormLabel>
+              <FormControl>
+                <MultiSelect
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  options={[
+                    { label: "2022", value: "2022" },
+                    { label: "2023", value: "2023" },
+                    { label: "2024", value: "2024" },
+                  ]}
+                />
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit">Aplicar</Button>
       </form>
     </Form>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,70 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const UFS = [
+  { uf: "AC", codigo: "12" },
+  { uf: "AL", codigo: "27" },
+  { uf: "AP", codigo: "16" },
+  { uf: "AM", codigo: "13" },
+  { uf: "BA", codigo: "29" },
+  { uf: "CE", codigo: "23" },
+  { uf: "DF", codigo: "53" },
+  { uf: "ES", codigo: "32" },
+  { uf: "GO", codigo: "52" },
+  { uf: "MA", codigo: "21" },
+  { uf: "MT", codigo: "51" },
+  { uf: "MS", codigo: "50" },
+  { uf: "MG", codigo: "31" },
+  { uf: "PA", codigo: "15" },
+  { uf: "PB", codigo: "25" },
+  { uf: "PR", codigo: "41" },
+  { uf: "PE", codigo: "26" },
+  { uf: "PI", codigo: "22" },
+  { uf: "RJ", codigo: "33" },
+  { uf: "RN", codigo: "24" },
+  { uf: "RS", codigo: "43" },
+  { uf: "RO", codigo: "11" },
+  { uf: "RR", codigo: "14" },
+  { uf: "SC", codigo: "42" },
+  { uf: "SP", codigo: "35" },
+  { uf: "SE", codigo: "28" },
+  { uf: "TO", codigo: "17" },
+];
+
+export const selectUFOptions = [
+  { label: "AC", value: "AC" },
+  { label: "AL", value: "AL" },
+  { label: "AP", value: "AP" },
+  { label: "AM", value: "AM" },
+  { label: "BA", value: "BA" },
+  { label: "CE", value: "CE" },
+  { label: "DF", value: "DF" },
+  { label: "ES", value: "ES" },
+  { label: "GO", value: "GO" },
+  { label: "MA", value: "MA" },
+  { label: "MT", value: "MT" },
+  { label: "MS", value: "MS" },
+  { label: "MG", value: "MG" },
+  { label: "PA", value: "PA" },
+  { label: "PB", value: "PB" },
+  { label: "PR", value: "PR" },
+  { label: "PE", value: "PE" },
+  { label: "PI", value: "PI" },
+  { label: "RJ", value: "RJ" },
+  { label: "RN", value: "RN" },
+  { label: "RS", value: "RS" },
+  { label: "RO", value: "RO" },
+  { label: "RR", value: "RR" },
+  { label: "SC", value: "SC" },
+  { label: "SP", value: "SP" },
+  { label: "SE", value: "SE" },
+  { label: "TO", value: "TO" },
+];
+
+export const findUFCodigo = async (uf: string) => {
+  return UFS.find(item => item.uf === uf)!.codigo;
+};
