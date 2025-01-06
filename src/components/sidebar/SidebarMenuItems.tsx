43cc4a136b6102d7bb5ac7eb0081eff6b400e281
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2025-01-06 16:05:38 -0300

    Refactor SidebarMenuItems to support dynamic rendering of FormContribuintes and FormConsultas based on selected tab

--- src/components/sidebar/SidebarMenuItems.tsx
+++ src/components/sidebar/SidebarMenuItems.tsx
@@ -2,25 +2,29 @@ import { SidebarMenu, SidebarMenuItem } from "@/components/ui/sidebar";
 import { ITEMS } from "./menu_items";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { FormContribuintes } from "@/forms/form-contribuintes/FormContribuintes";
+import { FormContribuintes, FormConsultas } from "@/forms";
 import { useDashboardState } from "@/state/dashboardState";
 
 export const SidebarMenuItems = () => {
   const { tabValue } = useDashboardState();
-  return (
-    <SidebarMenu className="px-4 ">
-      {tabValue === "contribuintes" ? (
-        <FormContribuintes />
-      ) : (
-        ITEMS.map(item => (
+
+  const renderForm = () => {
+    switch (tabValue) {
+      case "contribuintes":
+        return <FormContribuintes />;
+      case "consultas":
+        return <FormConsultas />;
+      default:
+        return ITEMS.map(item => (
           <SidebarMenuItem key={item.label} className="mb-4">
             <Label className="text-green font-bold">{item.label}</Label>
             <Input className="bg-white" placeholder={item.placeholder} />
           </SidebarMenuItem>
-        ))
-      )}
-    </SidebarMenu>
-  );
+        ));
+    }
+  };
+
+  return <SidebarMenu className="px-4 ">{renderForm()}</SidebarMenu>;
 };
 
 export default SidebarMenuItems;
