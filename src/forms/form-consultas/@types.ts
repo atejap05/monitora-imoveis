29be6de7ec3c3fc0d515fd7ad25f6fb8ecac5ec2
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2025-01-07 09:39:35 -0300

    Add NfseData interface to define structure for NFSe data in FormConsultas

---
+++ src/forms/form-consultas/@types.ts
@@ -0,0 +1,20 @@
+export interface NfseData {
+  nro_nfse: number;
+  valor_liq: number;
+  ano: number;
+  ni_raiz_tomador: string;
+  municipio: string;
+  chave_acesso: string;
+  loc_prestacao: string;
+  ni_tomador: string;
+  local_emissao: string;
+  valor_servico: number;
+  tomador: string;
+  servico_nacional: string;
+  ni_prestador: string;
+  ni_raiz_pretador: string;
+  mes: number;
+  nbs: string;
+  descricao_servico: string;
+  municipio_tomador: string;
+}
