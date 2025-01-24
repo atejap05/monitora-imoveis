e7da6aa9e44ef586f76ff2832c83e39cb99fd6c6
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2025-01-24 18:37:10 -0300

    Refactor NotasFiscais component to prepare and display chart data for PieChartNFSe by year

--- src/dashboards/NotasFiscais/NotasFiscais.tsx
+++ src/dashboards/NotasFiscais/NotasFiscais.tsx
@@ -1,4 +1,4 @@
-// import { TabsContent } from "@/components/ui/tabs";
+import { TabsContent } from "@/components/ui/tabs";
 // import DashCard from "@/components/DashCard";
 // import DashBarChart from "@/components/DashBarChart";
 // import {
@@ -10,15 +10,56 @@
 // } from "@/components/ui/card";
 import { useNotasFiscaisState } from "@/state/notasFiscaisState";
 import { PieChartNFSe } from "./PieChartNFSe";
-
+type ChartData = Array<{
+  emitente: string;
+  total: number;
+  fill: string;
+}>;
 const NotasFiscais = () => {
   const { consultaNFSeTotais } = useNotasFiscaisState();
+
+  // Preparar os dados para o PieChart. Sera um chart por ano com os totais de NFSe
+
+  const chartData = Object.entries(consultaNFSeTotais).map(([year, data]) => {
+    const chartData: ChartData = Object.entries(data)
+      .filter(([emitente]) => emitente !== "total")
+      .map(([emitente, total]) => {
+        let fill = "";
+        switch (emitente) {
+          case "mei":
+            fill = "hsl(var(--chart-2))";
+            break;
+          case "me_epp":
+            fill = "hsl(var(--chart-3))";
+            break;
+          case "nao_optante":
+            fill = "hsl(var(--chart-1))";
+            break;
+        }
+        return {
+          emitente,
+          total,
+          fill,
+        };
+      });
+    return {
+      year,
+      data: chartData,
+    };
+  });
+
+  console.log(chartData);
   return (
-    <>
+    <TabsContent value="nfse" className=" pl-4 py-6">
       <div>nfse</div>
       <div>{JSON.stringify(consultaNFSeTotais)}</div>
-      <PieChartNFSe />
-    </>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:gap-4">
+        {chartData.map((chart, index) => (
+          <PieChartNFSe key={index} chartData={chart.data} ano={chart.year} />
+        ))}
+      </div>
+    </TabsContent>
+
     // <TabsContent value="nfse" className=" pl-4 py-6">
     //   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 lg:gap-6">
     //     <DashCard
