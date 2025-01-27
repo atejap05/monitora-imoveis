02e9adc80bef21d5c4266ad500c32350ed15c637
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2025-01-27 11:06:32 -0300

    Add loading state to Notas Fiscais component with HashLoader

--- src/dashboards/NotasFiscais/NotasFiscais.tsx
+++ src/dashboards/NotasFiscais/NotasFiscais.tsx
@@ -9,6 +9,7 @@ import { TabsContent } from "@/components/ui/tabs";
 //   CardTitle,
 // } from "@/components/ui/card";
 import { useNotasFiscaisState } from "@/state/notasFiscaisState";
+import { HashLoader } from "react-spinners";
 import { PieChartNFSe } from "./PieChartNFSe";
 type ChartData = Array<{
   emitente: string;
@@ -16,10 +17,9 @@ type ChartData = Array<{
   fill: string;
 }>;
 const NotasFiscais = () => {
-  const { consultaNFSeTotais } = useNotasFiscaisState();
+  const { consultaNFSeTotais, isPending } = useNotasFiscaisState();
 
   // Preparar os dados para o PieChart. Sera um chart por ano com os totais de NFSe
-
   const chartData = Object.entries(consultaNFSeTotais).map(([year, data]) => {
     const chartData: ChartData = Object.entries(data)
       .filter(([emitente]) => emitente !== "total")
@@ -48,105 +48,30 @@ const NotasFiscais = () => {
     };
   });
 
-  console.log(chartData);
   return (
     <TabsContent value="nfse" className=" pl-4 py-6">
       <div>nfse</div>
-      <div>{JSON.stringify(consultaNFSeTotais)}</div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:gap-4">
-        {chartData.map((chart, index) => (
-          <PieChartNFSe key={index} chartData={chart.data} ano={chart.year} />
-        ))}
-      </div>
-    </TabsContent>
 
-    // <TabsContent value="nfse" className=" pl-4 py-6">
-    //   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 lg:gap-6">
-    //     <DashCard
-    //       title="Notas Fiscais Emitidas"
-    //       value={data ? String(data["2022"]) : "0"}
-    //       description="Notas fiscais emitidas por MEI em 2022"
-    //     />
-    //     <DashCard
-    //       title="Notas Fiscais Emitidas"
-    //       value={data ? String(data["2022"]) : "0"}
-    //       description="Notas fiscais emitidas por MEI em 2022"
-    //     />
-    //     <DashCard
-    //       title="Notas Fiscais Emitidas"
-    //       value={data ? String(data["2023"]) : "0"}
-    //       description="Notas fiscais emitidas por MEI em 2023"
-    //     />
-    //     <DashCard
-    //       title="Notas Fiscais Emitidas"
-    //       value={data ? String(data["2024"]) : "0"}
-    //       description="Notas fiscais emitidas por MEI em 2024"
-    //     />
-    //     <DashCard
-    //       title="Notas Fiscais Emitidas"
-    //       value={data ? String(data["2024"]) : "0"}
-    //       description="Notas fiscais emitidas por MEI em 2024"
-    //     />
-    //   </div>
-    //   <div className="grid grid-cols-1 sm:grid-cols-2  lg:gap-6 mt-6">
-    //     <Card>
-    //       <CardHeader>
-    //         <CardTitle>Notas Fiscais Emitidas por MEI</CardTitle>
-    //         <CardDescription>
-    //           Anos de {data ? Object.keys(data).join(", ") : ""}
-    //         </CardDescription>
-    //       </CardHeader>
-    //       <CardContent>
-    //         <DashBarChart
-    //           chartData={
-    //             data
-    //               ? Object.entries(data).map(([ano, value]) => ({
-    //                   ano,
-    //                   NFSE: value,
-    //                 }))
-    //               : []
-    //           }
-    //           dataKeyX="ano"
-    //           barDataKey="NFSE"
-    //           chartConfig={{
-    //             NFSE: {
-    //               label: "Notas Fiscais Emitidas",
-    //               color: "#709f77",
-    //             },
-    //           }}
-    //         />
-    //       </CardContent>
-    //     </Card>
-    //     <Card>
-    //       <CardHeader>
-    //         <CardTitle>Notas Fiscais Emitidas por MEI</CardTitle>
-    //         <CardDescription>
-    //           Anos de {data ? Object.keys(data).join(", ") : ""}
-    //         </CardDescription>
-    //       </CardHeader>
-    //       <CardContent>
-    //         <DashBarChart
-    //           chartData={
-    //             data
-    //               ? Object.entries(data).map(([ano, value]) => ({
-    //                   ano,
-    //                   NFSE: value,
-    //                 }))
-    //               : []
-    //           }
-    //           dataKeyX="ano"
-    //           barDataKey="NFSE"
-    //           chartConfig={{
-    //             NFSE: {
-    //               label: "Notas Fiscais Emitidas",
-    //               color: "#709f77",
-    //             },
-    //           }}
-    //         />
-    //       </CardContent>
-    //     </Card>
-    //   </div>
-    // </TabsContent>
+      {isPending ? (
+        <div className="flex flex-col justify-center items-center gap-3 h-96">
+          <HashLoader
+            loading={isPending}
+            color="#709f77"
+            aria-label="Loading ..."
+          />
+
+          <span className="text-green animate-pulse">
+            Consultado o Receita Data ...
+          </span>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 lg:gap-4">
+          {chartData.map((chart, index) => (
+            <PieChartNFSe key={index} chartData={chart.data} ano={chart.year} />
+          ))}
+        </div>
+      )}
+    </TabsContent>
   );
 };
 
