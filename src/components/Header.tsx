934038f82a1c9346f68cdb0e983928a73a13bc8d
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2025-01-27 11:53:55 -0300

    Refactor Header component to remove user data fetching and display Persona component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,8 @@
-import { formataCPF } from "@/lib/utils";
 import logonfse from "../assets/logo-nfse-horizontal-removebg.png";
+import Persona from "./Persona";
 import { Separator } from "./ui/separator";
-import { useQuery } from "@tanstack/react-query";
-
-type DadosUsuarioAutenticado = {
-  cpf: string;
-  email: string;
-  nome: string;
-  matricula: string;
-  unidade_cod: string;
-};
-
-const getDadosUsuarioAutenticado = async () => {
-  const url =
-    "https://localhost:8443/ctx/once/PainelNFSe/get_dados_usuario_autenticado";
-  const response = await fetch(url);
-  const data = await response.json();
-  return data as DadosUsuarioAutenticado;
-};
 
 const Header = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["dadosUsuarioAutenticado"],
-    queryFn: getDadosUsuarioAutenticado,
-  });
-
   return (
     <div className="flex items-center justify-between p-4 md:p-6 shadow-md sticky top-0 bg-[#fafafa] z-50">
       <div className="flex items-center gap-8">
@@ -41,14 +19,7 @@ const Header = () => {
           PAINEL NFSe
         </p>
       </div>
-      <div className="flex flex-col items-end gap-1">
-        <p className="text-sm sm:text-base md:text-lg text-gray-600">
-          {isLoading ? "Carregando..." : data?.nome}
-        </p>
-        <p className="text-xs sm:text-sm md:text-base text-gray-400">
-          {isLoading ? "Carregando..." : formataCPF(data?.cpf ?? "")}
-        </p>
-      </div>
+      <Persona />
     </div>
   );
 };
