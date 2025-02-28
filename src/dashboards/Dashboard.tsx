be410e28bc0a28c045ff026589b81ced1c55b99a
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2025-02-28 17:33:33 -0300

    Fix import path for VisaoGeral component in Dashboard to ensure correct module resolution

--- src/dashboards/Dashboard.tsx
+++ src/dashboards/Dashboard.tsx
@@ -2,7 +2,7 @@ import Ambiente from "./Ambiente";
 import Consultas from "./Consultas/Consultas";
 import Contribuintes from "./Contribuintes";
 import NotasFiscais from "./NotasFiscais/NotasFiscais";
-import VisaoGeral from "./VisaoGeral";
+import VisaoGeral from "./VisaoGeral/VisaoGeral";
 
 type DashboardProps = {
   tabValue: string;
