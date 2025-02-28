420b790221dbaa7b1af2dd669a4debfe5a06c1b7
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2025-02-28 17:33:50 -0300

    Add BasicLineChart component for frequency distribution visualization

---
+++ src/dashboards/VisaoGeral/BasicLineChart.tsx
@@ -0,0 +1,83 @@
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+const chartData = [
+  { faixa: "0-500", frequencia: 5000, mobile: 80 },
+  { faixa: "501-1000", frequencia: 305, mobile: 200 },
+  { faixa: "1001-1500", frequencia: 237, mobile: 120 },
+  { faixa: "1501-2000", frequencia: 73, mobile: 190 },
+  { faixa: "2001-2500", frequencia: 209, mobile: 130 },
+  { faixa: "2501-3000", frequencia: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  frequencia: {
+    label: "Frequência",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
+
+export function BasicLineChart() {
+  return (
+    <Card className="flex-1">
+      <CardHeader>
+        <CardTitle>Histograma</CardTitle>
+        <CardDescription>
+          Histograma de distribuição de frequência
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            data={chartData}
+            margin={{
+              left: 24,
+              right: 24,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="faixa"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              // tickFormatter={value => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Line
+              dataKey="frequencia"
+              type="natural"
+              stroke="#709f77"
+              strokeWidth={2}
+              dot={{
+                fill: "#709f77",
+              }}
+              activeDot={{
+                r: 6,
+              }}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
